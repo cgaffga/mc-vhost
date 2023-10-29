190268df642958a997dae40381504d25b3e69f00
Christoph <cgaffga@triplemind.com>
2023-10-29 20:37:56 +0000

    push

---
+++ src/main/java/net/gaffga/minecraft/VirtualHostCommandPlugin.java
@@ -0,0 +1,129 @@
+/*
+ * Created on 29 Oct 2023
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package net.gaffga.minecraft;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author cgaffga
+ *
+ *         TODO To change the template for this generated type comment go to
+ *         Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class VirtualHostCommandPlugin extends JavaPlugin implements Listener {
+
+	/**
+	 * vhost configs form config.yml.
+	 */
+	protected Map<String, ConfigurationSection> vhostConfigs = new HashMap<String, ConfigurationSection>();
+
+	/**
+	 * Map of loogin player name and their vhost (hostname they joined with).
+	 */
+	protected Map<String, String> playerLoginHosts = new HashMap<String, String>();
+
+	/**
+	 * Run when the plugin is loaded
+	 */
+	@Override
+	public void onEnable() {
+		getLogger().info("enabled!");
+
+		// register events froom this plugin to be loaded...
+		getServer().getPluginManager().registerEvents(this, this);
+
+		// read config.yml...
+		File vhostConfigFile = new File(getDataFolder(), "config.yml");
+		YamlConfiguration yaml = YamlConfiguration
+				.loadConfiguration(vhostConfigFile);
+
+		// go for all configured vhost sections...
+		ConfigurationSection vhosts = yaml.getConfigurationSection("vhosts");
+		if (vhosts != null) {
+			for (String key : vhosts.getKeys(false)) {
+				getLogger().info("vhost: " + key);
+
+				ConfigurationSection hostConfig = vhosts
+						.getConfigurationSection(key);
+
+				// add to vhost configs...
+				String hostname = hostConfig.getString("hostname");
+				getLogger().info(" - hostname:" + hostname);
+				vhostConfigs.put(hostname, hostConfig);
+
+			} // NEXT vhost.
+		}
+
+	}
+
+	/**
+	 * Catch event when player logs in. This is the event that provides us with
+	 * the hosname the player used to join the server, we keep that in our map.
+	 */
+	@EventHandler
+	public void onLogin(PlayerLoginEvent event) {
+		Player player = event.getPlayer();
+		getLogger().info("Player " + player.getName()
+				+ " is logging in to host " + event.getHostname());
+
+		// add player and hoostname to map...
+		playerLoginHosts.put(player.getName(), event.getHostname());
+	}
+
+	/**
+	 * Catch event when player finally joins. This is the point in time when we
+	 * can run commands for the player.
+	 */
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		String hostname = playerLoginHosts.get(player.getName());
+
+		getLogger().info("Player " + player.getName() + " is joining via host "
+				+ hostname);
+
+		// find a matching vhoost config...
+		ConfigurationSection vhostConfig = null;
+		for (String hostConfName : vhostConfigs.keySet()) {
+			if (hostname.startsWith(hostConfName)) {
+				vhostConfig = vhostConfigs.get(hostConfName);
+			}
+		}
+
+		// if there is a config fir this host...
+		if (vhostConfig != null) {
+			getLogger().info("vhost config found.");
+			// get the list of commands to run...
+			List<String> commands = vhostConfig.getStringList("commands");
+
+			// run all commands...
+			for (String command : commands) {
+				getLogger().info("run vhost commands: " + command);
+				// replace %player% placeholder with realk player name...
+				command = command.replace("%player%", player.getName());
+			    getLogger().info(" -  vhost commands: " + command);
+				
+				// run command on console...
+				getServer().dispatchCommand(getServer().getConsoleSender(),
+						command);
+			} // NEXT command.
+		}
+	}
+
+}
---
+++ src/main/java/net/gaffga/minecraft/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author cgaffga
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package net.gaffga.minecraft;
